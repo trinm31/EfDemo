9f819d344b670364a2038caab4b21f1f386c08c9
trinm31 <trinmgcd19833@gmail.com>
2021-04-07 10:45:36 +0700

    view data from db

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,23 @@
+﻿using EntityFramworkDemo.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntityFramworkDemo.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public StudentsController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var student = _db.Students.ToList();
+            return View(student);
+        }
+    }
+}
